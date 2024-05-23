f3a16b4c7e586688f78002f9c77c94ff62687f1c
Omkar Patil <ommipatil13@gmail.com>
2024-05-23 17:09:37 +0530

    build

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ app.post('/api/create-checkout-session', async (req, res) => {
         payment_method_types: ["card"],
         line_items: lineItems,
         mode: 'payment',
-        success_url: 'http://localhost:3000/success',
-        cancel_url: 'http://localhost:3000/cancel'
+        success_url: 'https://ecommerce-payment.onrender.com',
+        cancel_url: 'https://ecommerce-payment.onrender.com'
     })
 
     res.json({ id: session.id })

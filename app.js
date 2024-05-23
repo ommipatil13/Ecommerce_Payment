23286a85e6f2012436964618dc42f74481b4e51d
Omkar Patil <ommipatil13@gmail.com>
2024-05-23 15:48:52 +0530

    new

--- app.js
+++ app.js
@@ -34,11 +34,11 @@ app.post('/api/create-checkout-session', async (req, res) => {
 })
 
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+// app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", function (req, res) {
+//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 
 app.listen(7000, () => {
